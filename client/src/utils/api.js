d2c71544367de911034d2989ac5c0937a3047ba1
Andrew Chau <andrewchau333@gmail.com>
2021-02-27 09:39:31 -0800

    updating backend url for deployment

--- client/src/utils/api.js
+++ client/src/utils/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-var prefix="https://advance-x.herokuapp.com/"
+var prefix="https://advance-x.herokuapp.com"
 // prefix = "http://localhost:4000" // uncomment for local testing
 
 export default {
