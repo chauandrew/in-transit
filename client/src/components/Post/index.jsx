46e7185a7fc73cd932f16e4cec267ab7dce95cee
Andrew Chau <andrewchau333@gmail.com>
2022-04-04 23:00:24 -0400

    Replaced text on screen with modals

--- client/src/components/Post/index.jsx
+++ client/src/components/Post/index.jsx
@@ -1,35 +1,62 @@
-import React from 'react';
+import { React, useState } from "react";
 
-import {Col, Badge} from 'react-bootstrap'
-import PropTypes from 'prop-types'
+import { Col, Badge, Modal} from "react-bootstrap";
+import PropTypes from "prop-types";
 
-import './style.css' 
+import "./style.css";
 
-const Post = ({author, bodyText, date, imgUrls, tags}) => {
-    let tagBadges = []
-    for (const tag of tags) {
-        tagBadges.push(
-            <Badge className="post-tag" variant="info">
-                <a style={{"color": "white"}}href={"/filter/" + tag}>{tag}</a>
-            </Badge>
-        )
-    }
-    return (
-        <Col sm={12} md={6} lg={4}>
-            <img src={imgUrls[0]} className="post-img" alt={author+"-photo"}/>
-            <p>{bodyText}</p>
-            <h6>{author}</h6>
-            {/* <p>{date}</p> */}
-            <p>{tagBadges}</p>
-        </Col>
-    )
-}
+const Post = ({ author, bodyText, date, imgUrls, tags }) => {
+  const [modalShow, setModalShow] = useState(false);
+  let tagBadges = [];
+  let i = 0;
+  for (const tag of tags) {
+    tagBadges.push(
+      <Badge className="post-tag" variant="info" key={i}>
+        <a style={{ color: "white" }} href={"/filter/" + tag}>
+          {tag}
+        </a>
+      </Badge>
+    );
+    i++;
+  }
+  return (
+    <>
+      <Col className="post-container" sm={12} md={6} lg={4}>
+        <a
+          href="#"
+          onClick={(ev) => {
+            ev.preventDefault();
+            setModalShow(true);
+          }}
+        >
+          <img src={imgUrls[0]} className="post-img" alt={author + "-photo"} />
+        </a>
+        <h3 className="centered">{author.toUpperCase()}</h3>
+        {/* <p>{date}</p> */}
+        <p className="bottom-right">{tagBadges}</p>
+      </Col>
+      <Modal
+        show={modalShow}
+        onHide={() => setModalShow(false)}
+        size="lg"
+        aria-labelledby="contained-modal-title-vcenter"
+        centered
+      >
+        <img src={imgUrls} className="img-fluid" /> 
+        <Modal.Body>
+          <p>{bodyText}</p>
+          <h6><i>{author}</i></h6>
+        </Modal.Body>
+      </Modal>
+    </>
+  );
+};
 Post.propTypes = {
-    author: PropTypes.string.isRequired,
-    bodyText: PropTypes.string.isRequired,
-    date: PropTypes.string.isRequired,
-    imageUrls: PropTypes.array,
-    tags: PropTypes.array
-}
+  author: PropTypes.string.isRequired,
+  bodyText: PropTypes.string.isRequired,
+  date: PropTypes.string.isRequired,
+  imageUrls: PropTypes.array,
+  tags: PropTypes.array,
+};
 
 export default Post;
