8fdfc2c37296e5392459aaaa91effb844285eb9f
Andrew Chau <andrewchau333@gmail.com>
2022-05-09 11:15:21 -0700

    Fixed filter pages

--- client/src/components/FilteredPosts/index.jsx
+++ client/src/components/FilteredPosts/index.jsx
@@ -1,14 +1,31 @@
 import { React, useState, useEffect } from "react";
-import { Container, Row, Col } from "react-bootstrap";
+import { Container, Row, Col, Button } from "react-bootstrap";
 import { useParams } from "react-router";
 import Post from "../Post";
 import api from "../../utils/api";
 
 const FilteredPosts = () => {
-  const [posts, setPosts] = useState([]);
   const { tags } = useParams();
+  const [posts, setPosts] = useState([]);
+  const [displayedPosts, setDisplayedPosts] = useState([]);
+  const [pageNum, setPageNum] = useState(0);
+  const pageSize = 6;
 
-  console.log(tags);
+  const nextPage = () => {
+    if ((pageNum + 1) * pageSize < posts.length) {
+      setPageNum(pageNum + 1);
+    }
+  };
+  const prevPage = () => {
+    if (pageNum > 0) {
+      setPageNum(pageNum - 1);
+    }
+  };
+  useEffect(() => {
+    let front = pageNum * pageSize;
+    let back = front + pageSize;
+    setDisplayedPosts(posts.slice(front, back));
+  }, [pageNum, posts]);
 
   useEffect(() => {
     api.getPostsByTags(tags).then((res) => {
@@ -23,6 +40,7 @@ const FilteredPosts = () => {
             key={i}
             author={row[keymap["author"]]}
             bodyText={row[keymap["body"]]}
+            quote={row[keymap["quote"]]}
             date={row[keymap["createdDate"]]}
             imgUrls={row[keymap["imgUrls"]].split(",")}
             tags={row[keymap["tags"]].split(",")}
@@ -32,7 +50,7 @@ const FilteredPosts = () => {
       }
       setPosts(p);
     });
-  }, [tags]);
+  }, []);
 
   return (
     <Container>
@@ -41,7 +59,29 @@ const FilteredPosts = () => {
           <h2>{tags} Stories:</h2>
         </Col>
       </Row>
-      <Row>{posts}</Row>
+      <Row>{displayedPosts}</Row>
+
+      <Row className="m-3">
+        <Col lg={12} className="text-center">
+          <Button className="m-1" href="/">
+            Home
+          </Button>
+          {pageNum !== 0 ? (
+            <Button className="m-1" onClick={prevPage} variant="outline-dark">
+              Previous
+            </Button>
+          ) : (
+            ""
+          )}
+          {(pageNum + 1) * pageSize < posts.length ? (
+            <Button className="m-1" onClick={nextPage} variant="outline-dark">
+              Next
+            </Button>
+          ) : (
+            ""
+          )}
+        </Col>
+      </Row>
     </Container>
   );
 };
