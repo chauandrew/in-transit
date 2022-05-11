9df638e9173e41332b7a2780ca205bffd720d84e
Andrew Chau <andrewchau333@gmail.com>
2022-05-11 09:54:26 -0700

    Updated landing banner design and text

--- client/src/components/LandingBanner/index.jsx
+++ client/src/components/LandingBanner/index.jsx
@@ -4,22 +4,22 @@ import { Container, Row, Col } from "react-bootstrap";
 
 const LandingBanner = () => {
   return (
-    <div className="hero-image pt-5 pb-3 mb-5">
+    <div className="hero-image mb-5">
       <div className="hero-text pt-5 pb-5">
         <Container className="pt-5">
           <Row>
-            <Col lg={12}>
+            <Col className="text-md-right text-sm-left" md={3}>
               <h1>in:transit</h1>
             </Col>
-          </Row>
-          <Row>
-            <Col lg={12}>
+            <Col md={9}>
               <p>
-                My friends and I began meeting and interviewing college students
-                in Boston as a passion-project in 2021. I found that everyone
-                has a story to share, and no matter how put-together people
-                seem, we all experience struggle and brokenness in life. Here's
-                a collection of the stories we've heard.
+                <i>
+                  My friends and I began meeting and interviewing college
+                  students in Boston as a passion-project in 2021. I found that
+                  everyone has a story to share, and no matter how put-together
+                  people seem, we all experience struggle and brokenness in
+                  life. Here's a collection of the stories we've heard.
+                </i>
               </p>
             </Col>
           </Row>
