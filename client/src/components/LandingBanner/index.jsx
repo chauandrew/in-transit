9e619fef39e4783b5cbb48f67b184fbf9862c567
Andrew Chau <andrewchau333@gmail.com>
2022-05-12 20:05:35 -0700

    Added floating arrow on landing banner

--- client/src/components/LandingBanner/index.jsx
+++ client/src/components/LandingBanner/index.jsx
@@ -3,6 +3,13 @@ import "./style.css";
 import { Container, Row, Col } from "react-bootstrap";
 
 const LandingBanner = () => {
+  let downArrow = (
+    <svg className="arrow" x="0px" y="0px" viewBox="0 0 500 500">
+      <path d="M111,187.4c-7.6,6.6-7.6,17.3,0,23.9l116.2,101.2c7.6,6.6,19.8,6.6,27.4,0l116.2-101.2 c3.8-3.3,5.7-7.6,5.7-11.9c0-4.3-1.9-8.6-5.7-11.9c-7.6-6.6-19.8-6.6-27.4,0l-102.5,89.3l-102.5-89.3 C130.8,180.9,118.6,180.9,111,187.4z"></path>
+    </svg>
+  );
+  let url = window.location.href.split("#")[0] + "#post-container"; // so multiple clicks don't stack #...#...#..
+
   return (
     <div className="hero-image">
       <div className="hero-text pt-5 pb-5">
@@ -25,6 +32,7 @@ const LandingBanner = () => {
           </Row>
         </Container>
       </div>
+      <a href={url}>{downArrow}</a>
     </div>
   );
 };
